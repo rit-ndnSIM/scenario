bc9681c9bb2f3ccc4c0b6a8fb58d44a2c3332915
Carlos Barrios <cabeee@rit.edu>
2024-01-05 18:18:17 -0500

    updated run script to include configuration.

--- dag_run.sh
+++ dag_run.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
 
+
+#----- THIS ONLY NEEDS TO BE RUN ONCE --------------------
+#cd ~/ndnSIM/scenario
+#export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+#export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+#./waf clean
+#CXXFLAGS="-std=c++17" ./waf configure --debug
+#---------------------------------------------------------
+
+
+
+
 #NS_LOG=CustomAppConsumer:CustomAppProducer:DagForwarderApp:ndn.App ./waf --run=ndn-cabeee-3node-apps
 
 NS_LOG=CustomAppConsumer:CustomAppConsumer2:CustomAppProducer:DagForwarderApp:ndn.App ./waf --run=ndn-cabeee-3node-apps_reuse
