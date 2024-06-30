01f5d10131c183baad6a1ccc8194f62c7e0d448a
Carlos Barrios <cabeee@rit.edu>
2024-06-30 01:13:24 -0400

    small log trace file name mistake.

--- scenarios/ndn-cabeee-20node-linear.cpp
+++ scenarios/ndn-cabeee-20node-linear.cpp
@@ -324,8 +324,8 @@ main(int argc, char* argv[])
 
   Simulator::Stop(Seconds(2.0));
 
-  ndn::L3RateTracer::InstallAll("rate-trace_cabeee-20-linear.txt", Seconds(1.0));
-  ndn::CsTracer::InstallAll("cs-trace_cabeee-20-linear.txt", Seconds(1.0));
+  ndn::L3RateTracer::InstallAll("rate-trace_cabeee-20node-linear.txt", Seconds(1.0));
+  ndn::CsTracer::InstallAll("cs-trace_cabeee-20node-linear.txt", Seconds(1.0));
 
   Simulator::Run();
   Simulator::Destroy();
