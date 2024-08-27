cd2f099c3e6c19690323ff7fd41991fd8de5d976
Carlos Barrios <cabeee@rit.edu>
2024-08-27 14:06:39 -0400

    changes to install script to configure scenarios properly.

--- install_ndnSIM.sh
+++ install_ndnSIM.sh
@@ -13,12 +13,17 @@ git clone --recursive git@github.com:rit-ndnSIM/ndnSIM.git ns-3/src/ndnSIM
 git clone git@github.com:rit-ndnSIM/scenario.git scenario
 
 cd ns-3
-./waf configure -d debug --enable-examples --disable-python
+#./waf configure -d debug --enable-examples --disable-python
+./waf configure -d optimized --enable-examples --disable-python
 ./waf
 sudo ./waf install
 
 # now we can run scenarios
 cd ../scenario
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+CXXFLAGS="-std=c++17" ./waf configure --debug
+
 #./dag_run.sh
 
 
