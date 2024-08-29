35af99fc39a98aa7fc257dfbb1a8e1ce4a3840b2
Ethan Alker <eda5693@rit.edu>
2024-08-29 17:05:35 -0400

    update install script

--- install_ndnSIM.sh
+++ install_ndnSIM.sh
@@ -1,33 +1,29 @@
-#! /bin/bash
+#!/bin/bash
 
-INSTALL_DIR=~
-#INSTALL_DIR=/opt/ndn/
+# used to setup the dev environment
 
-cd $INSTALL_DIR
-if [ ! -d "ndnSIM" ]; then
-  mkdir ndnSIM
-fi
-cd ndnSIM
+set -eou pipefail
 
-git clone git@github.com:rit-ndnSIM/ns-3-dev.git ns-3
-git clone git@github.com:rit-ndnSIM/pybindgen.git pybindgen
-git clone --recursive git@github.com:rit-ndnSIM/ndnSIM.git ns-3/src/ndnSIM
+INSTALL_DIR="$HOME/ndnSIM"
 
-git clone git@github.com:rit-ndnSIM/scenario.git scenario
+mkdir -p "$INSTALL_DIR"
 
-cd ns-3
-#./waf configure -d debug --enable-examples --disable-python
-./waf configure -d optimized --enable-examples --disable-python
+# cloning through ssh, so make sure your user has read/write access
+git clone git@github.com:rit-ndnSIM/ns-3-dev.git "$INSTALL_DIR/ns-3"
+git clone git@github.com:rit-ndnSIM/pybindgen.git "$INSTALL_DIR/pybindgen"
+git clone --recursive git@github.com:rit-ndnSIM/ndnSIM.git "$INSTALL_DIR/ns-3/src/ndnSIM"
+
+git clone git@github.com:rit-ndnSIM/scenario.git "$INSTALL_DIR/scenario"
+
+cd "$INSTALL_DIR/ns-3"
+./waf configure -d debug --enable-examples --disable-python
 ./waf
 sudo ./waf install
 
-# now we can run scenarios
-cd ../scenario
+# now we can run scenarios, if we want
+cd "$INSTALL_DIR/scenario"
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
 CXXFLAGS="-std=c++17" ./waf configure --debug
 
 #./dag_run.sh
-
-
-
