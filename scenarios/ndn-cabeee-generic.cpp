9a8f789c654d666f50ab1f17038f5a4f6521009a
Ethan Alker <eda5693@rit.edu>
2025-03-17 18:10:16 -0400

    WIP: write generic scenario cpp

---
+++ scenarios/ndn-cabeee-generic.cpp
@@ -0,0 +1,151 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/**
+ * Copyright (c) 2011-2015  Regents of the University of California.
+ *
+ * This file is part of ndnSIM. See AUTHORS for complete list of ndnSIM authors and
+ * contributors.
+ *
+ * ndnSIM is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * ndnSIM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ndnSIM, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+// ndn-custom-apps.cpp
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/ndnSIM-module.h"
+#include "ns3/string.h"
+
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+#define SCENARIO_FILE "4dag.json"
+
+namespace ns3 {
+
+int
+main(int argc, char* argv[])
+{
+    // Read optional command-line parameters (e.g., enable visualizer with ./waf --run=<> --visualize
+    CommandLine cmd;
+    cmd.Parse(argc, argv);
+
+    // eventually get SCENARIO_FILE from args
+    std::ifstream f{ SCENARIO_FILE };
+    const json scenario_json = json::parse(f);
+
+    // for now, read topofile from scenario json
+    // eventually, build topology manually from json
+    // or maybe just generate topo file from json
+    std::string topofile = scenario_json.at("topofile");
+
+    // Creating nodes
+    AnnotatedTopologyReader topologyReader("", 1);
+    topologyReader.SetFileName(topofile);
+    topologyReader.Read();
+
+    // Install NDN stack on all nodes
+    ndn::StackHelper ndnHelper;
+    ndnHelper.setPolicy("nfd::cs::lru");
+    ndnHelper.SetDefaultRoutes(true);
+
+    map<std::string, const json*> srv_map{};
+    map<std::string, const json*> rtr_map{};
+    map<std::string, const json*> hosting_map{};
+
+    for (const auto& rtr : scenario_json.at("router")) {
+        std::string name{ rtr.at("node") };
+        Ptr<Node> node = Names::Find<Node>(name);
+        rtr_map[name] = &rtr;
+        // default to 1000
+        int cs_size = 1000;
+        if (rtr.contains("cs-size")) {
+            cs_size = rtr["cs-size"];
+        }
+        ndnHelper.setCsSize(cs_size);
+        ndnHelper.Install(node);
+    }
+
+    std::string Prefix{ scenario_json.at("prefix") };
+
+    for (const auto& srv : scenario_json.at("services")) {
+        std::string strategy{ "/localhost/nfd/strategy/mulicast" };
+        std::string name = srv.at("name");
+        srv_map[name] = &srv;
+        if (srv.contains("strategy")) {
+            strategy = srv["strategy"];
+        }
+        ndn::StrategyChoiceHelper::InstallAll(Prefix + name, strategy);
+    }
+
+    // Installing global routing interface on all nodes
+    //ndn::GlobalRoutingHelper ndnGlobalRoutingHelper;
+    //ndnGlobalRoutingHelper.InstallAll();
+
+    // Installing applications
+
+    for (const auto& hosting : scenario_json.at("routerHosting")) {
+        std::string rtr_name{ hosting.at("router") };
+        std::string srv_name{ hosting.at("service") };
+        Ptr<Node> rtr_node = Names::Find<Node>(rtr_name);
+        hosting_map[rtr_name + srv_name] = &hosting;
+
+        std::string type = srv_map.at(srv_name)->at("type");
+
+        int start = 0;
+        //int end = 0;
+
+        if (hosting.contains("start")) {
+            start = hosting["start"];
+        }
+        
+        //if (hosting.contains("end")) {
+        //    end = hosting["end"];
+        //}
+
+        if (type == "producer") {
+            ndn::AppHelper appHelper("CustomAppProducer");
+            appHelper.SetPrefix(Prefix);
+            appHelper.SetAttribute("Service", StringValue(srv_name));
+            appHelper.Install(rtr_node).Start(Seconds(start));
+            //app.Start(Seconds(start));
+            //app.Stop(Seconds(end));
+        } else if (type == "service") {
+            ndn::AppHelper appHelper("DagForwarderApp");
+            appHelper.SetPrefix(Prefix);
+            appHelper.SetAttribute("Service", StringValue(srv_name));
+            appHelper.Install(rtr_node).Start(Seconds(start));
+        } else if (type == "consumer") {
+            ndn::AppHelper appHelper("CustomAppConsumer");
+            appHelper.SetPrefix(Prefix);
+            appHelper.SetAttribute("Service", StringValue(srv_name));
+            appHelper.SetAttribute("Workflow", StringValue(SCENARIO_FILE));
+            appHelper.SetAttribute("Orchestrate", UintegerValue(0));
+            appHelper.Install(rtr_node).Start(Seconds(start));
+        } 
+    }
+
+    Simulator::Stop(Seconds(2.0));
+
+    Simulator::Run();
+    Simulator::Destroy();
+
+    return 0;
+}
+
+} // namespace ns3
+
+int
+main(int argc, char* argv[])
+{
+    return ns3::main(argc, argv);
+}
